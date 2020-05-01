35f1ae5f20eac10645448572aeadf3309814a879
Shashidhar Reddy Javaji <50128649+shashidharjavaji@users.noreply.github.com>
2020-05-01 16:14:26 +0530

    Create may1st.py
    You are a product manager and currently leading a team to develop a new product. Unfortunately, the latest version of your product fails the quality check. Since each version is developed based on the previous version, all the versions after a bad version are also bad.
    
    Suppose you have n versions [1, 2, ..., n] and you want to find out the first bad one, which causes all the following ones to be bad.
    
    You are given an API bool isBadVersion(version) which will return whether version is bad. Implement a function to find the first bad version. You should minimize the number of calls to the API.
    Given n = 5, and version = 4 is the first bad version.
    
    call isBadVersion(3) -> false
    call isBadVersion(5) -> true
    call isBadVersion(4) -> true
    
    Then 4 is the first bad version.

---
+++ may1st.py
@@ -0,0 +1,20 @@
+# The isBadVersion API is already defined for you.
+# @param version, an integer
+# @return a bool
+# def isBadVersion(version):
+
+class Solution:
+    def firstBadVersion(self, n):
+        start=1
+        end=n
+        while start<end:
+            mid = (start+end)//2
+            if(isBadVersion(mid)):
+                end=mid
+            else:
+                start=mid+1
+        return start
+        """
+        :type n: int
+        :rtype: int
+        """
